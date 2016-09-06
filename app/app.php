d27365cc96df782328b3c03707de0bba5cae876a
Katy Henning and Stephen Burden <stephen@>
2016-09-06 13:14:10 -0700

    add goodbye page

--- app/app.php
+++ app/app.php
@@ -21,10 +21,14 @@
                 <p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>
                 <p>Cheers,</p>
                 <p>Travel Enthusiast Katy</p>
+                <a href='/goodbye'>Goodbye!</a>
             </div>
         </body>
-        </html>"
-        ;
+        </html>";
+    });
+
+    $app->get("/goodbye", function() {
+        return "Goodbye friend!";
     });
 
     return $app;
